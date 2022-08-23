937c6465605a4f4db5420ee902e9add1721b8ee4
Vitaliy <70472034+Beginner1349@users.noreply.github.com>
2022-08-23 20:26:28 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = '127.0.0.1';
-$username = 'root';
-$password = 'pass';
-$db_name = 'testdb';
-$dsn = 'mysql:dbname=testdb;host=127.0.0.1';
-
-$pdo = new PDO($dsn, $username, $password);
-?>
